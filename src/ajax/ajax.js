c7e9e9d3673f66a57571b2cc821110477e36fff8
朱伟杰 <443984744@qq.com>
2019-01-23 15:27:12 +0800

    通过配置proxy代理，解决跨域问题

--- src/ajax/ajax.js
+++ src/ajax/ajax.js
@@ -1,43 +1,43 @@
-import axios from 'axios'
+import axios from "axios";
+// create-react-app的官网代理参数，会把当前的所有ajax请求代理到下面配置的host域名，防止跨域发生。  link: https://facebook.github.io/create-react-app/docs/proxying-api-requests-in-development
+
 // ajax
 function loginAjax(id, pwd) {
-    return new Promise((resolve, reject) => {
-        axios({
-            url: 'https://www.sanoind.com/api/user/login',
-            method: 'post',
-            data: {
-                id,
-                pwd
-            }
-        })
-            .then((res) => {
-                console.log(res)
-                resolve(res)
-            })
-            .then(
-                authorizationAjax()
-            )
-            .catch((err) => {
-                reject(err)
-            })
-    });
+  return new Promise((resolve, reject) => {
+    axios({
+      url: "/api/user/login",
+      method: "post",
+      data: {
+        id,
+        pwd
+      }
+    })
+      .then(res => {
+        console.log(res);
+        resolve(res);
+      })
+      .then(authorizationAjax)
+      .catch(err => {
+        reject(err);
+      });
+  });
 }
 
 // 登录状态
 function authorizationAjax() {
-    return new Promise((resolve, reject) => {
-        axios({
-            url: 'https://www.sanoind.com/api/user/authorization',
-            method: 'get',
-        })
-            .then((res) => {
-                console.log(res)
-                resolve(res)
-            })
-            .catch((err) => {
-                reject(err)
-            })
-    });
+  return new Promise((resolve, reject) => {
+    axios({
+      url: "/api/user/authorization",
+      method: "get"
+    })
+      .then(res => {
+        console.log(res);
+        resolve(res);
+      })
+      .catch(err => {
+        reject(err);
+      });
+  });
 }
 
-export default loginAjax
+export default loginAjax;
